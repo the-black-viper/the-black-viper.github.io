973b95527b380523fe9c36175c97bc7f598d5d05
Joshua Javier <joshua.javier.github@gmail.com>
2026-01-16 13:12:30 +0800

    updated vite config for analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-62P26TQJEK",
+      },
+    }),
+  ],
   base: "/",
 });
